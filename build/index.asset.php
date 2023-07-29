e6428ace4d6947090bfbad0bb5ab21d95e5b6595
UjW0L <bktujwol@gmail.com>
2023-07-29 14:27:20 +0545

    Block.json update

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '80103603e0f9b27800e1');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '75601ddee9d5efb3c22d');
